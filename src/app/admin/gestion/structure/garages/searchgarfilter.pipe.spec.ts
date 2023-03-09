87bccaae9ccd82e60f443f82a11cef165769d1c0
Dago Herve Jean-Jacques GNAORE <hgnaore@COD-LAP-DSI-HG.local>
2023-03-09 16:15:55 +0000

    Finalisation Structure/Garage

---
+++ src/app/admin/gestion/structure/garages/searchgarfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchgarfilterPipe } from './searchgarfilter.pipe';
+
+describe('SearchgarfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchgarfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
